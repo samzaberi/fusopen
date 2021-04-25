2cc4ca1236cdfa91a0a21a14411a6afe1d9f758b
samzaberi <samz.aberi@gmail.com>
2021-04-25 20:08:47 +0300

    removed commented variables

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -38,12 +38,7 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  // // const part1 = 'Fundamentals of React'
-  // const exercises1 = 10
-  // // const part2 = 'Using props to pass data'
-  // const exercises2 = 7
-  // // const part3 = 'State of a component'
-  // const exercises3 = 14
+
 
   const content = {
     part1: 'Fundamentals of React',
