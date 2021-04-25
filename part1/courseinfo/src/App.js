ec4cd1a820bc30c03782ba77a9f27abcaf7e7cd2
samzaberi <samz.aberi@gmail.com>
2021-04-25 20:07:27 +0300

    part1 excercise 1.1 and 1.2

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,67 @@
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Part = (props) => {
+
+  return (
+    <div>
+      <p>
+        {props.part} {props.exercises}
+      </p>
+    </div>
+  )
+}
+
+const Content = (props) => {
+
+  return (
+    <div>
+      <Part part={props.cont.part1} exercises={props.cont.exercises1} />
+      <Part part={props.cont.part2} exercises={props.cont.exercises2} />
+      <Part part={props.cont.part3} exercises={props.cont.exercises3} />
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>Number of exercises {props.total}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  // // const part1 = 'Fundamentals of React'
+  // const exercises1 = 10
+  // // const part2 = 'Using props to pass data'
+  // const exercises2 = 7
+  // // const part3 = 'State of a component'
+  // const exercises3 = 14
+
+  const content = {
+    part1: 'Fundamentals of React',
+    exercises1: 10,
+    part2: 'Using props to pass data',
+    exercises2: 7,
+    part3: 'State of a component',
+    exercises3: 14
+  }
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content cont={content} />
+      <Total total={content.exercises1 + content.exercises2 + content.exercises3} />
+    </div>
+
+  )
+}
+
+export default App;
