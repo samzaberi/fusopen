30e6167c54332d6ba74995fc120e875d9310a78d
samzaberi <samz.aberi@gmail.com>
2021-04-27 15:28:20 +0300

    ex1.5 code

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -38,22 +38,7 @@ const Total = (props) => {
 }
 
 const App = () => {
-  // const course = 'Half Stack application development'
 
-  // const parts = [
-  //   {
-  //     name: 'Fundamentals of React',
-  //     exercises: 10
-  //   },
-  //   {
-  //     name: 'Using props to pass data',
-  //     exercises: 7
-  //   },
-  //   {
-  //     name: 'State of a component',
-  //     exercises: 14
-  //   }
-  // ]
   const course = {
     name: 'Half Stack application development',
     parts: [
