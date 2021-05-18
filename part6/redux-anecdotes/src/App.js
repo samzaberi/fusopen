f2467a79a3c636232c7eb77c3484f0e0e657d2a9
samzaberi <samz.aberi@gmail.com>
2021-05-18 11:42:19 +0300

    added ex 6.5

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -21,7 +21,7 @@ const App = () => {
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
+      {anecdotes.sort((a, b) => b - a).map(anecdote =>
         <div key={anecdote.id}>
           <div>
             {anecdote.content}
