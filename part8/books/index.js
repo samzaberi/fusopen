c74d09dc499b8689ea692e5773410e3caadcc48a
samzaberi <samz.aberi@gmail.com>
2021-05-24 11:11:08 +0300

    added ex 8.4

--- part8/books/index.js
+++ part8/books/index.js
@@ -100,7 +100,7 @@ type Author {
   type Query {
     bookCount:Int!
     authorCount:Int!
-    allBooks:[Book!]!
+    allBooks(author:String):[Book!]!
     allAuthors:[Author!]!
   }
 `
@@ -109,7 +109,12 @@ const resolvers = {
     Query: {
         bookCount: () => books.length,
         authorCount: () => authors.length,
-        allBooks: () => books,
+        allBooks: (root, args) => {
+            if (!args.author) {
+                return books
+            }
+            return books.filter(b => b.author === args.author)
+        },
         allAuthors: () => {
             const result = authors.map(author => {
                 let count = books.reduce((a, b) => b.author === author.name ? a += 1 : a, 0)
