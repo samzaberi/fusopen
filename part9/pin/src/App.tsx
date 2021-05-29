9cac23c1820bf7144516569642b07e8f4ec13e01
samzaberi <samz.aberi@gmail.com>
2021-05-29 16:21:34 +0300

    added ex9.15

--- part9/pin/src/App.tsx
+++ part9/pin/src/App.tsx
@@ -1,25 +1,90 @@
 import React from 'react';
 
+interface CoursePartBase {
+  name: string;
+  exerciseCount: number;
+  type: string;
+}
+
+interface CourseWithDescPart extends CoursePartBase {
+  description: string
+}
+
+interface CourseNormalPart extends CourseWithDescPart {
+  type: "normal";
+}
+interface CourseProjectPart extends CoursePartBase {
+  type: "groupProject";
+  groupProjectCount: number;
+}
+
+interface CourseSubmissionPart extends CourseWithDescPart {
+  type: "submission";
+  exerciseSubmissionLink: string;
+}
+
+// interface course {
+//   name: string,
+//   exerciseCount: number
+// }
+
+// interface courseProps {
+//   courses: Array<course>
+// }
+
+type CoursePart = CourseNormalPart | CourseProjectPart | CourseSubmissionPart;
+
+const assertNever = (value: never): never => {
+  throw new Error(
+    `Unhandled discriminated union member: ${JSON.stringify(value)}`
+  );
+};
+
 const Header = ({ courseName }: { courseName: string }) => {
   return <h1>{courseName}</h1>
 }
 
-interface course {
-  name: string,
-  exerciseCount: number
+const CourseDetails = ({ course }: { course: CoursePart }) => {
+  switch (course.type) {
+    case "normal":
+      return (
+        <div>
+          <p>{course.name}<br />
+            {course.type}<br />
+            {course.description}<br />
+            {course.exerciseCount}</p>
+        </div>
+      )
+    case "groupProject":
+      return (
+        <div>
+          <p>{course.name}<br />
+            {course.type}<br />
+            {course.exerciseCount}<br />
+            {course.groupProjectCount}</p>
+        </div>
+      )
+    case "submission":
+      return (
+        <div>
+          <p>{course.name}<br />
+            {course.type}<br />
+            {course.exerciseCount}<br />
+            {course.exerciseSubmissionLink}</p>
+        </div>
+      )
+    default:
+      return assertNever(course);
+  }
 }
 
-interface courseProps {
-  courses: Array<course>
-}
 
-
-const Content = ({ courses }: courseProps) => {
+const Content = ({ courses }: { courses: CoursePart[] }) => {
   return (
     <div>
       {
         courses.map(c =>
-          <p key={c.name}>{c.name} {c.exerciseCount}</p>
+          <CourseDetails key={c.name} course={c} />
         )
       }
     </div>
@@ -27,7 +92,7 @@ const Content = ({ courses }: courseProps) => {
   )
 }
 
-const Total = ({ courses }: courseProps) => {
+const Total = ({ courses }: { courses: CoursePart[] }) => {
   return (
     <div>
       <p>
@@ -38,20 +103,35 @@ const Total = ({ courses }: courseProps) => {
   )
 }
 
+
+
 const App = () => {
   const courseName = "Half Stack application development";
-  const courseParts = [
+  const courseParts: CoursePart[] = [
     {
       name: "Fundamentals",
-      exerciseCount: 10
+      exerciseCount: 10,
+      description: "This is the leisured course part",
+      type: "normal"
+    },
+    {
+      name: "Advanced",
+      exerciseCount: 7,
+      description: "This is the harded course part",
+      type: "normal"
     },
     {
       name: "Using props to pass data",
-      exerciseCount: 7
+      exerciseCount: 7,
+      groupProjectCount: 3,
+      type: "groupProject"
     },
     {
       name: "Deeper type usage",
-      exerciseCount: 14
+      exerciseCount: 14,
+      description: "Confusing description",
+      exerciseSubmissionLink: "https://fake-exercise-submit.made-up-url.dev",
+      type: "submission"
     }
   ];
 
