e4cf2dcc4f525fdf678a2172e7fea6064997b935
samzaberi <samz.aberi@gmail.com>
2021-05-31 11:54:25 +0300

    added ex 9.17

---
+++ part9/patientor/src/components/PatientDetails.tsx
@@ -0,0 +1,40 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { apiBaseUrl } from "../constants";
+import { Patient } from "../types";
+
+
+const PatientDetails = () => {
+    const [patient, setPatient] = useState<Patient>();
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const id: string = useParams<{ id: string }>().id;
+
+    useEffect(() => {
+        const fetchPatient = async () => {
+            try {
+                const { data: patient } = await axios.get<Patient>(`${apiBaseUrl}/patients/${id}`);
+                setPatient(patient);
+            } catch (error) {
+                console.error(error.message);
+            }
+        };
+        void fetchPatient();
+    });
+
+
+    return (
+        <div>
+            <h2>patient details</h2>
+            <div>
+                name: {patient?.name}<br />
+                birthday: {patient?.dateOfBirth}<br />
+                social security: {patient?.ssn}<br />
+                gender: {patient?.gender}<br />
+                occupation: {patient?.occupation}<br />
+            </div>
+        </div>
+    );
+};
+
+export default PatientDetails;
